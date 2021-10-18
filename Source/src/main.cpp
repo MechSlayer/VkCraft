f9c371d3c09e4ae7c2c8d08d9fb03b3f1d3450f9
SkillerRaptor <skillerraptor@protonmail.com>
2021-10-18 12:08:29 +0200

    VkCraft: Initial Code - Adding basic CMake - Adding CMake utils - Adding main.cpp

---
+++ Source/src/main.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020-2021, SkillerRaptor <skillerraptor@protonmail.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <cstdint>
+
+auto main() -> int32_t
+{
+	return 0;
+}
